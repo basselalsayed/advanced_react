143ddfa055a4c54877af2573a6d052b8a92838e1
basselalsayed <alsayed.bassel@gmail.com>
2020-04-25 16:12:35 +0100

    updated app to use state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import Person from './Person/Person'
 
 export default class App extends Component {
+  state = {
+    persons: [
+      { name: 'Tom', age: 28 },
+      { name: 'Dick', age: 25 },
+      { name: 'Harry', age: 22 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked')
+  }
+
   render() {
     return (
       <div className="App">
-        <Person name='Tom' age='28'  />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}  />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}  />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
         <Person name='Dick' age='25'>My Hobbies: Racing</Person>
-        <Person name='Harry' age='22'  />
       </div>
     );
   }
