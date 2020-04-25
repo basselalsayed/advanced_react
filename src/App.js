01cbdd6650c2a90227e4df1a21d4806c45ffc68d
basselalsayed <alsayed.bassel@gmail.com>
2020-04-25 15:59:38 +0100

    updated person to use children and props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Person />
+        <Person name='Tom' age='28'  />
+        <Person name='Dick' age='25'>My Hobbies: Racing</Person>
+        <Person name='Harry' age='22'  />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const person = () => {
+const person = (props) => {
+  
   return (
-    <p>
-      I'm a person!
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   )
 }
 
