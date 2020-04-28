2b2b5b3117d62f4206f8ba05493136956ae3e9f8
basselalsayed <alsayed.bassel@gmail.com>
2020-04-28 17:19:10 +0100

    added two way binding for dynamic updating

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ export default class App extends Component {
     ],
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Dick', age: 25 },
+        { name: 'James', age: 25 },
+      ],
+    });
+  };
+
+  nameChangedHandler = (event) => {
     this.setState({
       persons: [
         { name: 'Tom', age: 28 },
-        { name: 'Dick', age: 25 },
+        { name: event.target.value, age: 25 },
         { name: 'James', age: 25 },
       ],
     });
@@ -24,24 +34,25 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Bas!!')}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
         >
           My Hobbies: Racing
         </Person>
         <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
         />
-        <Person name="Dick" age="25">
-          My Hobbies: Racing
-        </Person>
       </div>
     );
   }
